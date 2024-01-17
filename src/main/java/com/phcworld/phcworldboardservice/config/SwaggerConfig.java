0844dcae2e2cff35ac89b1e672dab11cacf82ff4
javamogi@gmail.com <40781237+javamogi@users.noreply.github.com>
2024-01-17 11:21:16 +0900

    feat: swagger 추가

---
+++ src/main/java/com/phcworld/phcworldboardservice/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.phcworld.phcworldboardservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "PHC-WORLD BOARDS Service API 명세서",
+                description = "PHC-WORLD RESTful level2 API 명세서 입니다.",
+                version = "v1.0.0"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi customTestOpenAPi() {
+        String[] paths = {"/boards/**"};
+
+        return GroupedOpenApi
+                .builder()
+                .group("일반 사용자 Board 도메인에 대한 API")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
--- src/main/java/com/phcworld/phcworldboardservice/web/FreeBoardApiController.java
+++ src/main/java/com/phcworld/phcworldboardservice/web/FreeBoardApiController.java
@@ -5,6 +5,8 @@ import com.phcworld.phcworldboardservice.dto.FreeBoardRequestDto;
 import com.phcworld.phcworldboardservice.dto.FreeBoardResponseDto;
 import com.phcworld.phcworldboardservice.dto.FreeBoardSearchDto;
 import com.phcworld.phcworldboardservice.service.FreeBoardService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +23,9 @@ public class FreeBoardApiController {
 
     private final FreeBoardService freeBoardService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", description = "DB에 저장할 때 UNIQUE 충돌")
+    })
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public FreeBoardResponseDto registerBoard(@RequestBody FreeBoardRequestDto requestDto, HttpServletRequest request) {
@@ -34,6 +39,10 @@ public class FreeBoardApiController {
         return freeBoardService.getSearchList(search, token);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "요청 게시글 없음"),
+            @ApiResponse(responseCode = "409", description = "삭제된 게시글")
+    })
     @GetMapping("/{freeBoardId}")
     public ResponseEntity<Map<String, Object>> getFreeBoard(@PathVariable(name = "freeBoardId") String freeBoardId,
                                                             HttpServletRequest request){
@@ -42,6 +51,11 @@ public class FreeBoardApiController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "요청 게시글 없음"),
+            @ApiResponse(responseCode = "409", description = "삭제된 게시글"),
+            @ApiResponse(responseCode = "403", description = "수정 권한 없음")
+    })
     @PatchMapping("")
     public FreeBoardResponseDto updateBoard(@RequestBody FreeBoardRequestDto requestDto,
                                             HttpServletRequest request) {
@@ -49,6 +63,11 @@ public class FreeBoardApiController {
         return freeBoardService.updateFreeBoard(requestDto, token);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "요청 게시글 없음"),
+            @ApiResponse(responseCode = "409", description = "삭제된 게시글"),
+            @ApiResponse(responseCode = "403", description = "삭제 권한 없음")
+    })
     @DeleteMapping("/{boardId}")
     public SuccessResponseDto deleteBoard(@PathVariable(name = "boardId") String boardId){
         return freeBoardService.deleteFreeBoard(boardId);
